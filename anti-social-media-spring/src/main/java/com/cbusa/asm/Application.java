47aa4d439adcd060be9579f7d6b99ec5568d6789
Cole Busa <cole.busa@gmail.com>
2023-09-30 23:01:25 -0400

    Added MySQL database connection and some basic REST interactions.

--- anti-social-media-spring/src/main/java/com/cbusa/asm/Application.java
+++ anti-social-media-spring/src/main/java/com/cbusa/asm/Application.java
@@ -2,11 +2,16 @@ package com.cbusa.asm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories( "com.cbusa.asm.**")
+@ComponentScan(basePackages = { "com.cbusa.asm.**"})
+@EntityScan( "com.cbusa.asm.**" )
 public class Application {
-
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
--- anti-social-media-spring/src/main/java/com/cbusa/asm/UserController.java
+++ anti-social-media-spring/src/main/java/com/cbusa/asm/UserController.java
@@ -1,14 +1,17 @@
 package com.cbusa.asm;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+	@Autowired
+	private UserRepository userRepository;
 
 	@GetMapping("/")
-	public String index() {
-		return "Test!";
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
 	}
 
 }
