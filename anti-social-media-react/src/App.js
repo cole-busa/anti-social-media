3a99fbafa28460b7467dfa14d095afa7f6dd4df8
Cole Busa <cole.busa@gmail.com>
2023-10-02 23:24:37 -0400

    Added a login page with POST

--- anti-social-media-react/src/App.js
+++ anti-social-media-react/src/App.js
@@ -5,6 +5,7 @@ import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 
+
 function App() {
   const [users, setUsers] = useState();
   const getUsers = async () => {
--- anti-social-media-react/src/components/home/Home.js
+++ anti-social-media-react/src/components/home/Home.js
@@ -1,9 +1,44 @@
 import React from 'react';
 
+import api from '../../api/axiosConfig'
+import { useState } from 'react';
+
 const Home = () => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        
+        const data = JSON.stringify({ username, password });
+        try {
+            api.post(
+                "/User/", 
+                data,
+                { headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true }
+            ).then(response => {console.log(response)});
+        } catch(e) {
+            console.log(e);
+        }
+      }
+    
+      const [username, setUsername] = useState()
+      const [password, setPassword] = useState()
+    
     return (
         <div>
-            Welcome!
+            <form action="" id="login" method="post">
+                <h1>Login</h1>
+                <p className="item">
+                    <label htmlFor="username"> Username </label>
+                    <input type="username" name="username" id="username" />
+                </p>
+                <p className="item">
+                    <label htmlFor="password"> Password </label>
+                    <input type="password" name="password" id="password" />
+                </p>
+                <p className="item">
+                    <input type="submit" value="Login" />
+                </p>
+            </form>
         </div>
     );
 }
