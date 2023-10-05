b310a2bd28da99abd70b30d48abfcb5efb73b4d8
Cole Busa <cole.busa@gmail.com>
2023-10-04 23:24:20 -0400

    Added MUI template to profile Home

--- anti-social-media-react/src/components/home/Home.js
+++ anti-social-media-react/src/components/home/Home.js
@@ -1,14 +1,88 @@
 import React, { useContext } from 'react';
 
 import { Context } from '../../GlobalVariables';
-import { Typography } from '@mui/material';
+import { AppBar, Box, Button, Card, CardActions, CardContent, CardMedia, Container, CssBaseline, Grid, Stack, ThemeProvider, Toolbar, Typography, createTheme } from '@mui/material';
 
 const Home = () => {
     const [currentUser, setCurrentUser] = useContext(Context);
+    const defaultTheme = createTheme();
+    const anti_social_score = 1;
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     return (
-        <div>
-            <Typography sx={{color:'white', fontFamily: 'Roboto', fontSize: '90px'}}> Welcome {currentUser}! </Typography>
-        </div>
+        <ThemeProvider theme={defaultTheme}>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <Typography variant="h6" color="inherit" noWrap>
+                        {currentUser}'s Page!
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Box
+                    sx={{
+                        bgcolor: 'background.paper',
+                        pt: 8,
+                        pb: 6,
+                    }}
+                >
+                    <Container maxWidth="sm">
+                        <Typography
+                            component="h1"
+                            variant="h2"
+                            align="center"
+                            color="text.primary"
+                            gutterBottom
+                        >
+                            {currentUser}'s Page!
+                        </Typography>
+                        <Typography variant="h5" align="center" color="purple" fontWeight='bold' paragraph>
+                            Anti-Social Score: {anti_social_score}
+                        </Typography>
+                        <Stack
+                            sx={{ pt: 4 }}
+                            direction="row"
+                            spacing={2}
+                            justifyContent="center"
+                        >
+                        </Stack>
+                    </Container>
+                </Box>
+                <Container sx={{ py: 8 }} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {cards.map((card) => (
+                            <Grid item key={card} xs={12} sm={6} md={4}>
+                                <Card
+                                    sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                >
+                                    <CardMedia
+                                        component="div"
+                                        sx={{
+                                            // 16:9
+                                            pt: '56.25%',
+                                        }}
+                                        image="https://source.unsplash.com/random?wallpapers"
+                                    />
+                                    <CardContent sx={{ flexGrow: 1 }}>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card. You can use this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size="small">View</Button>
+                                        <Button size="small">Edit</Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
+            </main>
+        </ThemeProvider>
     );
 }
 
