5998c4b28169212f1144ea1926d959453f95ea74
Cole Busa <cole.busa@gmail.com>
2023-10-14 21:31:58 -0400

    Started containerization of MySQL database

--- anti-social-media-spring/src/main/java/com/cbusa/asm/domain/Movie.java
+++ anti-social-media-spring/src/main/java/com/cbusa/asm/domain/Movie.java
@@ -1,10 +1,14 @@
 package com.cbusa.asm.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Movie")
 public class Movie {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- anti-social-media-spring/src/main/java/com/cbusa/asm/domain/VideoGame.java
+++ anti-social-media-spring/src/main/java/com/cbusa/asm/domain/VideoGame.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "VideoGame")
 public class VideoGame {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
